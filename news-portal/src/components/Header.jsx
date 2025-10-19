27e1f9cc00fb4e0fcc4113f1a0798e72eb85e834
antnose <antnose28@gmail.com>
2025-10-19 21:21:23 +0600

    Create a basic layout

---
+++ news-portal/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import logo from "../assets/logo.png";
+import { format } from "date-fns";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="flex justify-center py-5">
+        <img src={logo} alt="" />
+      </div>
+      <p className="text-center">Journalism Without Fear or Favour</p>
+
+      <p className="text-center">
+        {format(new Date(), "EEEE")}, {format(new Date(), "LLLL")}{" "}
+        {format(new Date(), "d")}, {format(new Date(), "Y")}
+      </p>
+    </div>
+  );
+};
+
+export default Header;
